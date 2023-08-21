4e29cca725592012ca69e207f452b4dae48008ef
mpearmain <michael.pearmain@gmail.com>
2023-08-21 17:57:34 +0700

    Adding placeholders for Thirdparty SDK

---
+++ src/thỉdparty_sdk/__init__.py
@@ -0,0 +1,4 @@
+from .specification import Specification, SpecificationData
+from .implementation import Implementation, ImplementationData
+from .infrastructure import Infrastructure, InfrastructureData
+from .thirdparty import Thirdparty
