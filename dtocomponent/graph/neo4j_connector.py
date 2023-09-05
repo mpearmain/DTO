f83d99cf221a7a04e8c8025ddd5f7dfee3ddcb09
mpearmain <michael.pearmain@gmail.com>
2023-09-05 17:05:45 +0700

    updates to parsers

--- dtocomponent/graph/neo4j_connector.py
+++ dtocomponent/graph/neo4j_connector.py
@@ -1,5 +1,5 @@
 from neo4j import GraphDatabase
-from typing import Dict, Any
+from typing import Dict, Any, List, Tuple
 
 
 class Neo4jConnector:
@@ -8,97 +8,132 @@ class Neo4jConnector:
     """
 
     def __init__(self, uri: str, user: str, password: str):
+        """
+        Initialize the Neo4j connector with connection details.
+
+        Parameters:
+        - uri (str): The connection URI for the Neo4j database.
+        - user (str): The username for the Neo4j database.
+        - password (str): The password for the Neo4j database.
+        """
         self._driver = GraphDatabase.driver(uri, auth=(user, password))
 
-    def test_connection(self):
+    def test_connection(self) -> None:
+        """Test the connection to the Neo4j database."""
         with self._driver.session() as session:
             result = session.run("RETURN 'Hello, World!' AS greeting")
             print(result.single()["greeting"])
 
     def close(self) -> None:
+        """Close the connection to the Neo4j database."""
         self._driver.close()
 
-    def write_component(self, component: Dict) -> None:
-        try:
-            with self._driver.session() as session:
-                session.write_transaction(self._write_component_tx, component)
-        except Exception as e:
-            print(f"Error writing to Neo4j: {e}")
+    def generate_cypher_queries(self, component: Dict[str, Any]) -> List[str]:
+        """
+        Generate the Cypher queries using the GraphBuilder.
+
+        Parameters:
+        - component (Dict[str, Any]): The input data structure.
+
+        Returns:
+        - List[str]: The list of generated Cypher queries.
+        """
+        builder = GraphBuilder()
+        return builder.generate_cypher(component)
+
+    def execute_cypher_queries(self, queries: List[str]) -> None:
+        """
+        Execute a list of Cypher queries on the Neo4j database.
+
+        Parameters:
+        - queries (List[str]): The list of Cypher queries to execute.
+        """
+        with self._driver.session() as session:
+            for query in queries:
+                session.run(query)
+
+
+class GraphBuilder:
+    """A class to generate Cypher commands from a given nested dictionary."""
 
     @staticmethod
-    def _write_component_tx(tx, component: Dict) -> None:
-        component_parser = ComponentParser(tx)
-        component_parser.parse(component)
-
-
-class ComponentParser:
-    """
-    A helper class to parse the component dictionary and create the corresponding nodes and relationships in Neo4j.
-    """
-
-    def __init__(self, tx):
-        self.tx = tx
-        self.base_parser = BaseParser(self.tx)
-
-    def parse(self, data: Dict[str, Any]):
-        # There's only one top-level key, the name of the component
-        component_name = list(data.keys())[0]
-        component_data = data[component_name]
-
-        for schema_type, schema_data in component_data.items():
-            node_id = self.base_parser._create_node({}, schema_type)
-            for key, value in schema_data.items():
-                self.base_parser._process_value(node_id, key, value)
-
-
-class BaseParser:
-    """
-    A base parser class that contains common methods for creating nodes and relationships in Neo4j.
-    """
-
-    def __init__(self, tx):
-        self.tx = tx
-
-    def _create_node(self, properties: Dict[str, Any], label: str) -> int:
+    def _create_node(label: str, properties: Dict[str, Any] = None) -> Tuple[str, str]:
         """
-        Create a node in the Neo4j database with the provided properties and label.
+        Create a node Cypher command.
+
+        Parameters:
+        - label (str): The label of the node.
+        - properties (Dict[str, Any], optional): The properties of the node.
+
+        Returns:
+        - Tuple[str, str]: The Cypher command and the alias of the node.
         """
-        props = {k: v for k, v in properties.items() if not isinstance(v, (dict, list))}
-        result = self.tx.run("CREATE (n:" + label + " $props) RETURN id(n)", props=props)
-        node_id = result.single()[0]
-        return node_id
-
-    def _create_relationship(self, parent_node_id: int, child_node_id: int, rel_type: str) -> None:
-        """
-        Create a relationship between two nodes in the Neo4j database.
-        """
-        self.tx.run(f"MATCH (p), (n) WHERE id(p) = $parent_id AND id(n) = $child_id CREATE (p)-[:{rel_type}]->(n)",
-                    parent_id=parent_node_id, child_id=child_node_id)
-
-    def parse(self, data: Dict[str, Any]):
-        # Create the root node (e.g., "Specification")
-        root_node = self._create_node({}, list(data.keys())[0])
-
-        # Process the inner dictionary (e.g., "Attributes" or "Dependencies")
-        for key, value in data[list(data.keys())[0]].items():
-            self._process_value(root_node, key, value)
-
-    def _process_value(self, parent_node, key, value):
-        if isinstance(value, dict):
-            # Check if the key is one of the special keys like 'Type', 'Configuration', etc.
-            if key in ['Type', 'Configuration', 'Resource', 'Details']:
-                child_node = self._create_node({}, key)  # Create a node with the key's name
-                self._create_relationship(parent_node, child_node, key)
-                for sub_key, sub_value in value.items():
-                    self._process_value(child_node, sub_key, sub_value)
-            else:
-                child_node = self._create_node({}, key)
-                self._create_relationship(parent_node, child_node, "HAS")
-                for sub_key, sub_value in value.items():
-                    self._process_value(child_node, sub_key, sub_value)
-        elif isinstance(value, list):
-            for item in value:
-                self._process_value(parent_node, key, item)
+        alias = label.replace(' ', '_').lower()
+        if properties:
+            props = ', '.join(f'{k}: "{v}"' if isinstance(v, str) else f'{k}: {v}' for k, v in properties.items())
+            return f'CREATE ({alias}:{label} {{{props}}})', alias
         else:
-            value_node = self._create_node({key: value}, key)
-            self._create_relationship(parent_node, value_node, key)
+            return f'CREATE ({alias}:{label})', alias
+
+    @staticmethod
+    def _create_relationship(parent_alias: str, child_alias: str, rel_type: str) -> str:
+        """
+        Create a relationship Cypher command.
+
+        Parameters:
+        - parent_alias (str): The alias of the parent node.
+        - child_alias (str): The alias of the child node.
+        - rel_type (str): The type of the relationship.
+
+        Returns:
+        - str: The Cypher command for the relationship.
+        """
+        return f'CREATE ({parent_alias})-[:{rel_type}]->({child_alias})'
+
+    def generate_cypher(self, data: Dict[str, Any], parent_alias: str = None, relationship: str = "Subcomponent") -> \
+    List[str]:
+        """
+        Generate Cypher commands based on the input data.
+
+        Parameters:
+        - data (Dict[str, Any]): The input data structure.
+        - parent_alias (str, optional): The alias of the parent node.
+        - relationship (str, optional): The type of the relationship.
+
+        Returns:
+        - List[str]: The list of generated Cypher commands.
+        """
+        queries = []
+
+        for key, value in data.items():
+            # Handle simple key-value attributes
+            if not isinstance(value, dict):
+                if parent_alias:
+                    properties = {'value': value}
+                    query, alias = self._create_node(key, properties=properties)
+                    queries.append(query)
+                    queries.append(self._create_relationship(parent_alias, alias, relationship))
+            # Handle "Type" keyword
+            elif "Type" in value:
+                node_label = value["Type"]
+                properties = {k: v for k, v in value.items() if k != "Type" and not isinstance(v, dict)}
+                query, alias = self._create_node(node_label, properties=properties)
+                queries.append(query)
+                if parent_alias:
+                    queries.append(self._create_relationship(parent_alias, alias, f'HAS_{key}'))
+                nested_queries = self.generate_cypher(value, parent_alias=alias)
+                queries.extend(nested_queries)
+            # Handle special keywords
+            elif key in ["Resources", "Configuration", "Details"]:
+                nested_queries = self.generate_cypher(value, parent_alias=parent_alias, relationship=f'HAS_{key}')
+                queries.extend(nested_queries)
+            else:
+                properties = {k: v for k, v in value.items() if not isinstance(v, dict)}
+                query, alias = self._create_node(key, properties=properties)
+                queries.append(query)
+                if parent_alias:
+                    queries.append(self._create_relationship(parent_alias, alias, relationship))
+                nested_queries = self.generate_cypher(value, parent_alias=alias)
+                queries.extend(nested_queries)
+
+        return queries
--- example/simple/pipeline/neo4j_load_pipeline_example_.py
+++ example/simple/pipeline/neo4j_load_pipeline_example_.py
@@ -37,23 +37,27 @@ implementation.load_instance(implementation_metadata)
 infrastructure.load_instance(infrastructure_metadata)
 
 # Create a Component instance
-pipeline = Component(name='datapipeline', specification=specification, implementation=implementation,
+pipeline = Component(name='DATA_PIPELINE_EXAMPLE', specification=specification, implementation=implementation,
                      infrastructure=infrastructure)
 
 # Get the combined configuration
+# Get the combined configuration
 configuration = pipeline.configure()
-print(list(configuration['datapipeline'].keys()))
 print(configuration)  # Print or use the combined configuration as needed
-
-# Load into neo4j and test out - MAKE SURE NEO4J is running!!!"
-
 
 # Connect to Neo4j and write the component configuration
 uri = "bolt://localhost:7687"  # Replace with your Neo4j URI
 user = "neo4j"  # Replace with your Neo4j username
 password = "test1234"  # Replace with your Neo4j password
 
+# Initialize the connector
 connector = Neo4jConnector(uri, user, password)
-print('running here')
-connector.write_component(configuration)
+
+# Generate the Cypher queries
+queries = connector.generate_cypher_queries(configuration)
+
+# Execute the Cypher queries on the Neo4j database
+connector.execute_cypher_queries(queries)
+
+# Close the connection
 connector.close()
