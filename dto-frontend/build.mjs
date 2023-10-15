725495563611a46c7020fa717a5fc6f91720544e
James Hay <pokinoice@live.com>
2023-10-15 12:31:53 +0800

    Updates

--- dto-frontend/build.mjs
+++ dto-frontend/build.mjs
@@ -1,7 +1,7 @@
 import esbuild from 'esbuild';
 
 esbuild.build({
-  entryPoints: ['./dto-frontend/src/main.ts'],
+  entryPoints: ['./dto-frontend/src/main.tsx'],
   bundle: true,
   platform: 'browser',
   entryNames: '[name].bundle',
